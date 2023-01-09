ca6dfe7209bc9306bb1c0d2809fb4e5454a3bb93
YRodin <rodin4doc@gmail.com>
2023-01-09 15:48:07 -0500

    create server folder structure, build user model

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
