3b5454bff9f05fc32f8b3b47ad97febfc9feb7c5
YRodin <rodin4doc@gmail.com>
2023-02-03 11:08:23 -0500

    debug user settings menu, ensure that backed api responds with uniform data format {userName, seasonPass, token}

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,44 +1,55 @@
-const jwt = require('jwt-simple')
-const User = require('../models/user')
-const keys = require('../config/dev')
+const jwt = require("jwt-simple");
+const User = require("../models/user");
+const keys = require("../config/dev");
 
 function tokenForUser(user) {
-  return jwt.encode({ sub: user.id,
-    iat: Math.round(Date.now() / 1000),
-    exp: Math.round(Date.now() / 1000 + 5 * 60 * 60)}, keys.TOKEN_SECRET)
+  return jwt.encode(
+    {
+      sub: user._id,
+      iat: Math.round(Date.now() / 1000),
+      exp: Math.round(Date.now() / 1000 + 5 * 60 * 60),
+    },
+    keys.TOKEN_SECRET
+  );
 }
+exports.tokenForUser = tokenForUser;
 
-exports.signin = function(req, res, next) {
-  res.send({
-    token: tokenForUser(req.user)
-  })
-}
+exports.signin = function (req, res, next) {
+  const token = tokenForUser(req.user);
+  const { userName, seasonPass } = req.user;
+  res.send({userName, seasonPass, token});
+};
 
-exports.currentUser = function(req, res, next) {
-  console.log(`current user controller is invoked`)
+exports.currentUser = function (req, res, next) {
+  console.log(`current user controller is invoked`);
   User.findById(req.user._id, (err, user) => {
-    if(err) { return next(err); }
+    if (err) {
+      return next(err);
+    }
     res.json(user);
-  })
-}
+  });
+};
 
-exports.signup = function(req, res, next) {
-  const userName = req.body.userName
-  const password = req.body.password
+exports.signup = function (req, res, next) {
+  const userName = req.body.userName;
+  const password = req.body.password;
 
-  console.log(userName)
 
   if (!userName || !password) {
-    return res.status(422).send({ error: 'You must provide userName and password'})
+    return res
+      .status(422)
+      .send({ error: "You must provide userName and password" });
   }
 
   // See if a user with the given userName exists
-  User.findOne({ userName: userName }, function(err, existingUser) {
-    if (err) { return next(err) }
+  User.findOne({ userName: userName }, function (err, existingUser) {
+    if (err) {
+      return next(err);
+    }
 
     // If a user with userName does exist, return an error
     if (existingUser) {
-      return res.status(422).send({ error: 'userName is in use' })
+      return res.status(422).send({ error: "userName is in use" });
     }
 
     // If a user with userName does NOT exist, create and save user record
@@ -48,11 +59,15 @@ exports.signup = function(req, res, next) {
 
     user.setPassword(password);
 
-    user.save(function(err, user) {
-      if (err) { return next(err) }
+    user.save(function (err, user) {
+      if (err) {
+        return next(err);
+      }
 
       // Repond to request indicating the user was created
-      res.json({ token: tokenForUser(user) })
+      const token = tokenForUser(user);
+      const  {userName, seasonPass} = user;
+      res.send({userName, seasonPass, token})
     });
   });
-}
+};
--- server/controllers/manageUser.js
+++ server/controllers/manageUser.js
@@ -1,53 +1,99 @@
-
-const User = require('../models/user')
+const authentication = require("./authentication");
+const User = require("../models/user");
 
 exports.addPass = function (req, res, next) {
   // find and update user in db
-  User.findOne({userName: req.user.userName}, function(err, user){
-    if(err) { next(err) }
-    else {
+  User.findOne({ userName: req.user.userName }, function (err, user) {
+    if (err) {
+      next(err);
+    } else {
       user.seasonPass = req.body.seasonPass;
-      user.save((err, user) =>{
-        if(err) { return next(err) }
-        res.json(user);
-      })
-    }
-  })  
-}
-
-exports.updateInfo = function (req, res, next) {
-  //update password? model.setPassword()
-  if(req.body.password) {
-    User.findById(req.user._id, (err, user) => {
-      if(err) { next(err); }
-      else {
-        user.setPassword(req.body.password);
-        user.save();
-      }
-    });
-  }
-  //udpdate user name or ski pass
-  User.findByIdAndUpdate(req.user._id, req.body, function(err, user) {
-    if(err) { res.status(500).json({ error: 'Failed to update account' })} 
-    else {
       user.save((err, user) => {
-        if (err) { console.log(err); }
-        else {
-          console.log(user);
-          res.json(user)
+        if (err) {
+          return next(err);
         }
+        res.json(user);
       });
     }
-  })
-}
+  });
+};
+
+exports.updateInfo = async (req, res, next) => {
+  const updatedUser = {
+    userName: "",
+    seasonPass: "",
+    token: "",
+  };
+  for (const prop in req.body) {
+    // loop over each property in request body and update user
+    switch (prop) {
+      case "password":
+        console.log("case password is invoked");
+        try {
+          const user = await User.findById(req.user._id);
+          user.setPassword(req.body.password);
+          await user.save();
+        } catch (err) {
+          next(err);
+        }
+        break;
+
+      case "userName":
+        //instructions
+        console.log(
+          `case userName is invoked with new user name: ${req.body.userName}`
+        );
+        try {
+          const user = await User.findByIdAndUpdate(req.user._id, req.body, {
+            new: true,
+          });
+          updatedUser.userName = user.userName;
+          await user.save();
+        } catch (err) {
+          next(err);
+        }
+        break;
+
+      case "seasonPass":
+        //instructions
+        console.log(
+          `case seasonPass is invoked with new seasonPass: ${req.body.seasonPass}`
+        );
+        try {
+          const user = await User.findByIdAndUpdate(req.user._id, req.body, {
+            new: true,
+          });
+          updatedUser.seasonPass = user.seasonPass;
+          await user.save();
+        } catch (err) {
+          next(err);
+        }
+        break;
+
+      default:
+        console.log("nothing is passed with req.body to update user!");
+    }
+  }
+    //generate new token for user
+    updatedUser.token = authentication.tokenForUser(req.user);
+
+    //send response back to client
+    res.send({
+      userName: updatedUser.userName,
+      seasonPass: updatedUser.seasonPass,
+      token: updatedUser.token,
+    });
+  }
 
 exports.delete = function (req, res, next) {
   // delete DB entry
   // redirect to /home
+  console.log("user delete api route is invoked");
   User.findByIdAndDelete(req.user._id, (err, user) => {
-    if (err) { return next(err) }
-    else {
-      res.status(204).json({ message: 'User deleted successfully'});
+    if (err) {
+      return next(err);
+    } else {
+      res.status(204).json({ message: "User deleted successfully" });
     }
-  })
-}
+  });
+};
